f57554b5e1b9fb955703e978eb72cb667484a512
xenon <cj.rdwn@gmail.com>
2021-01-17 02:26:43 +0600

    core module updated

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,9 +1,7 @@
-import { NgModule } from '@angular/core';
+import { NgModule,Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-
-
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
@@ -11,4 +9,13 @@ import { FooterComponent } from './components/footer/footer.component';
     CommonModule
   ]
 })
-export class CoreModule { }
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+    if (parentModule) {
+      throw new Error(
+        'CoreModule is already loaded. Import it in the AppModule only'
+      );
+    }
+  }
+}
+
