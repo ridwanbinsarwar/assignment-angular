1acd0b8b12037b22bf1ddcfd967b43ed8a29a39e
xenon <cj.rdwn@gmail.com>
2021-01-18 01:47:08 +0600

    custom password validator added

--- src/app/auth/components/registration/registration.component.ts
+++ src/app/auth/components/registration/registration.component.ts
@@ -15,13 +15,21 @@ export class RegistrationComponent implements OnInit {
       'firstName': new FormControl('',Validators.required ),
       'lastName': new FormControl('', Validators.required),
       'email': new FormControl('', [Validators.required, Validators.email, Validators.pattern('[a-z0-9.@]*')]),
-      'password': new FormControl('',Validators.required)
+      'password': new FormControl('',[Validators.required, this.validatePassword])
     })
   }
 
   onSubmit() {
     // TODO: Use EventEmitter with form value
     console.warn(this.registrationForm.value);
+  }
+
+
+   validatePassword(control: FormControl): {[key: string]: any} | null  {
+    if (control.value && control.value.length < 6) {
+      return { 'password length too short': true };
+    }
+    return null;
   }
 
 }
